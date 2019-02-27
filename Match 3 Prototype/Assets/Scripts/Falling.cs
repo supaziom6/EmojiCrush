9ffc860d64bf53ef5fc75e8885bac8c4d6e3e638
Mosherboy9 <Jasondawiddebeer1@gmail.com>
2019-02-27 22:13:26 +0000

    Main Menu Edited
    Added Emoji's Falling

--- Match 3 Prototype/Assets/Scripts/Falling.cs
+++ Match 3 Prototype/Assets/Scripts/Falling.cs
@@ -4,12 +4,61 @@ using UnityEngine;
 
 public class Falling : MonoBehaviour
 {
-    public List<GameObject> sprites;
-    public List<GameObject> toPool;
-    private int amountToPool = 10;
+    public Sprite[] Sprites;
+    public bool GoLeft;
+    public GameObject Sprite;
+    public Vector2 startPos;
+    public float Xposition;
+    public float YPosition;
+    public float rotation;
+    public float Speed;
 
     void Start()
     {
-        
+        GetComponent<SpriteRenderer>().sprite= Sprites[(Random.Range(0,Sprites.Length))];
+        if (Random.Range(0f, 1f) < 0.5f)
+        {
+            GoLeft = false;
+        }
+        else
+        {
+            GoLeft = true;
+        }
+        Xposition = Random.Range(-2.5f, 2.5f);
+        rotation = Random.Range(-40, 40);
+        Speed = Random.Range(1f, 4f);
+        YPosition = 339;
+        startPos = Sprite.transform.position;
+        Sprite.transform.Rotate(0,0,rotation);
+    }
+
+    void Update()
+    {
+        if(Sprite.transform.position.y < -5.5)
+        {
+            rotation = Random.Range(-40, 40);
+            Sprite.transform.Rotate(0, 0, rotation);
+            Xposition = Random.Range(-2.5f, 2.5f);
+            Speed = Random.Range(1f, 4f);
+            Sprite.transform.position = startPos;
+        }
+        Sprite.transform.Translate(Vector2.down * (Speed * Time.deltaTime), Space.World);
+
+        if(GoLeft)
+        {
+            Sprite.transform.Translate(Vector2.left * ((Speed*Random.Range(1f,2f)) * Time.deltaTime), Space.World);
+        }
+        else
+        {
+            Sprite.transform.Translate(Vector2.right * ((Speed * Random.Range(1f, 2f)) * Time.deltaTime), Space.World);
+        }
+        if (Sprite.transform.position.x < -2.50)
+        {
+            GoLeft = false;
+        }
+        if (Sprite.transform.position.x > 2.5)
+        {
+            GoLeft = true;
+        }
     }
 }
