11778a595ea408d464097efd0150101670768d84
Jason <JasonDawidDeBeer1@gmail.com>
2019-02-27 10:56:03 +0000

    Added Main Menu
    -Main menu added
    -function to launch proto from menu

---
+++ Match 3 Prototype/Assets/Scripts/Falling.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Falling : MonoBehaviour
+{
+    public List<GameObject> sprites;
+    public List<GameObject> toPool;
+    private int amountToPool = 10;
+
+    void Start()
+    {
+        
+    }
+}
---
+++ Match 3 Prototype/Assets/Scripts/MainMenuHandler.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuHandler : MonoBehaviour
+{
+    // Main Menu Buttons
+    public GameObject LevelButtons; // buttons for the levels
+    public GameObject Levels; // button to select levels
+    public GameObject ReturnButton;
+    public GameObject OptionsButton;
+    public GameObject ExitButton;
+    // Parent Objects
+    public GameObject OptionsMenu;
+    // Sprites
+    public GameObject BackgroundImage;
+    public GameObject OptionsBackground;
+
+    // Methods for Menu
+    public void LevelsMenu()
+    {
+        Levels.SetActive(false);
+        ExitButton.SetActive(false);
+        LevelButtons.SetActive(true);
+        OptionsBackground.SetActive(false);
+        ReturnButton.SetActive(true);
+    }
+    public void Options()
+    {
+        Levels.SetActive(false);
+        OptionsButton.SetActive(false);
+        ExitButton.SetActive(false);
+        LevelButtons.SetActive(false);
+        OptionsMenu.SetActive(true);
+        OptionsBackground.SetActive(true);
+        ReturnButton.SetActive(true);
+    }
+    public void Exit()
+    {
+        Application.Quit();
+    }
+    public void Return()
+    {
+        OptionsBackground.SetActive(false);
+        LevelButtons.SetActive(false);
+        Levels.SetActive(true);
+        OptionsButton.SetActive(true);
+        OptionsMenu.SetActive(false);
+        ExitButton.SetActive(true);
+        ReturnButton.SetActive(false);
+    }
+    public void Level()
+    {
+        SceneManager.LoadScene("Game");
+    }
+}
