be19b7c45e236fc94af8f58fb9882636be82ca9c
Mosherboy9 <Jasondawiddebeer1@gmail.com>
2019-03-04 20:53:15 +0000

    Shop Added
    Shop pop up added
    power up buttons added (not functional atm)
    cash text added

--- Match 3 Prototype/Assets/Scripts/UI/GameUI.cs
+++ Match 3 Prototype/Assets/Scripts/UI/GameUI.cs
@@ -5,8 +5,14 @@ using UnityEngine.SceneManagement;
 
 public class GameUI : MonoBehaviour
 {
-    private GameObject BackButton;
-    private GameObject PauseButton;
+    public GameObject BackButton;
+    public GameObject PauseButton;
+    public GameObject Score_Text;
+    public GameObject Cash_Text;
+    public GameObject Shop;
+    public GameObject Shop_Button;
+    public GameObject Power_Up1;
+    public GameObject Power_Up2;
     public void Pause()
     {
         BackButton.SetActive(true);
@@ -15,16 +21,27 @@ public class GameUI : MonoBehaviour
     public void Back()
     {
         BackButton.SetActive(false);
+        Shop.SetActive(false);
+        Shop_Button.SetActive(true);
+        Power_Up1.SetActive(false);
+        Power_Up2.SetActive(false);
         PauseButton.SetActive(true);
     }
-    public void ShowPowerUps()
+    public void ShowShop()
     {
-        BackButton.SetActive(false);
+        BackButton.SetActive(true);
+        Shop_Button.SetActive(false);
+        Shop.SetActive(true);
+        Power_Up1.SetActive(true);
+        Power_Up2.SetActive(true);
         PauseButton.SetActive(false);
     }
-
-    public void showUI(ButtonActionRequest[] itemsTurnedon)
+    public void PowerUp1()
     {
-        return;
+        //Do Something Here
+    }
+    public void PowerUp2()
+    {
+        //Do Something Here
     }
 }
