aa308b213a24a94e201c6a0012e2e5bedbb07c79
supaziom6 <adrian.michalowski4@gmail.com>
2019-03-05 22:28:56 +0000

    removed preset level while i work on a fix

--- Match 3 Prototype/Assets/Scripts/LevelEditing/LevelInfo.cs
+++ Match 3 Prototype/Assets/Scripts/LevelEditing/LevelInfo.cs
@@ -3,14 +3,26 @@ using System.Collections.Generic;
 using UnityEngine;
 
 [CreateAssetMenu(fileName = "LevelInfo", menuName = "LevelEditor", order = 0)]
-public class LevelInfo : ScriptableObject {
+[System.Serializable]
+public class LevelInfo : ScriptableObject 
+{
+
+	[System.Serializable]
+	public struct LevelEmojiInfo 
+	{
+		[Tooltip("Drag in the emoji prefab in here")]
+		public GameObject emojiType;
+		[Tooltip("Ammount will be added to a total and then devised as a percantage. Leave blank for even chance of spawn")]
+		[Range(0,1)]
+		public float spawnChance;
+	}
 
 	/// <Summary>
 	///	Defines the types of emoji that can spawn
 	/// </Summary>
 	[Tooltip("List of Emoji that can spawn in this level and their chances to spawn")]
 	public List<LevelEmojiInfo> possibleEmoji;
-
+	[Header("Board Size")]
 	[Tooltip("Size of the board hotizontally")]
 	[Range(5,25)]
 	public int xSize;
