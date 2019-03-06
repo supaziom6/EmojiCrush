cb4c63e328afe850484916cc483fccb6b8bd6ea8
Jason <JasonDawidDeBeer1@gmail.com>
2019-03-06 11:04:40 +0000

    Added TextManager
    Added Text Manager

--- Match 3 Prototype/Assets/Scripts/UI/TextManager.cs
+++ Match 3 Prototype/Assets/Scripts/UI/TextManager.cs
@@ -1,16 +1,59 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class TextManager : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    public TextMeshProUGUI ScoreText;
+    private int Score;
+    public TextMeshProUGUI CashText;
+    private int Cash;
+    public TextMeshProUGUI MovesLeft;
+    private int Moves;
+    public TextMeshProUGUI EmojisText;
+    private int Emojis;
+    public bool GoalReached;
+
+    private void Start()
+    {
+        GoalReached = false;
+
+        Score = 0;
+        /*
+         * load emoji from level file
+         * load move limit from level file
+         * 
+         * */
+
+
+        RegisterUpdate();
+
+    }
+
+    void RegisterUpdate()
+    {
+        ScoreText.text = "Score: " + Score;
+        EmojisText.text = "Emojis Left: " + Emojis;
+        CashText.text = "Cash: " + Cash;
+    }
+    public void GoalEmojis(int emojidestroyed)
+    {
+        Emojis -= emojidestroyed;
+        if(Emojis <= 0)
+        {
+            GoalReached = true;
+        }
+    }
+    public void UseMove()
+    {
+        Moves -= 1;
+        RegisterUpdate();
+    }
+
+    public void UpdateScore(int score)
+    {
+        Score += score;
+        RegisterUpdate();
+    }
 }
