4b65a5c9c9884880a9ffecca60865c2d6458d2ef
ronan-doherty <45420442+ronan-doherty@users.noreply.github.com>
2019-03-14 21:28:11 +0000

    Script for tutorial
    Script for tutorial written

---
+++ Match 3 Prototype/Assets/Scripts/UI/Tutorial.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tutorial : MonoBehaviour {
+
+    public GameObject MatchingContinue, UIContinue, Match4Continue, Match5Continue, PowerUpsContinue;
+    public GameObject tutorial_1, tutorial_2, tutorial_3, tutorial_4, tutorial_5;
+
+
+    public void TutorialStart()
+    {
+        tut_1();
+    }
+
+
+   public void tut_1()
+    {
+        tutorial_1.SetActive(true);
+        MatchingContinue.SetActive(true);
+    }
+
+    public void tut_2()
+    {
+        tutorial_1.SetActive(false);
+        MatchingContinue.SetActive(false);
+        tutorial_2.SetActive(true);
+        UIContinue.SetActive(true);
+    }
+
+    public void tut_3()
+    {
+        tutorial_2.SetActive(false);
+        UIContinue.SetActive(false);
+        tutorial_3.SetActive(true);
+        Match4Continue.SetActive(true);
+    }
+
+    public void tut_4()
+    {
+        tutorial_3.SetActive(false);
+        Match4Continue.SetActive(false);
+        tutorial_4.SetActive(true);
+        Match5Continue.SetActive(true);
+    }
+
+    public void tut_5()
+    {
+        Match5Continue.SetActive(false);
+        tutorial_4.SetActive(false);
+        tutorial_5.SetActive(true);
+        PowerUpsContinue.SetActive(true);
+    }
+
+    public void tut_over()
+    {
+        tutorial_5.SetActive(false);
+        PowerUpsContinue.SetActive(false);
+    }
+}
