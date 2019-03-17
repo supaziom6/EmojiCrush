9d587ae0b41afcf3a59491a06d6083cbbef53432
supaziom6 <adrian.michalowski4@gmail.com>
2019-03-17 16:31:20 +0000

    Created a save Manager and a Shop
     - Created save Manager to dynamically store data throught out the games install time
     - Created a shop that affects the save file by adding and removing data from it

---
+++ Match 3 Prototype/Assets/Scripts/SavingManager.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+using UnityEngine;
+
+public class SavingManager : MonoBehaviour {
+
+	public static SaveData PersistantData = new SaveData();
+
+	public void Save()
+	{
+		BinaryFormatter bf = new BinaryFormatter();
+		FileStream file = File.Create(Application.persistentDataPath + "/SaveFile.EmCr");
+
+		SaveData data = PersistantData;
+
+		bf.Serialize(file, data);
+		file.Close();
+	}
+
+	public void Load()
+	{
+		if(File.Exists(Application.persistentDataPath + "/SaveFile.EmCr"))
+		{
+			BinaryFormatter bf = new BinaryFormatter();
+			FileStream file = File.Open(Application.persistentDataPath + "/SaveFile.EmCr", FileMode.Open);
+			SaveData data = (SaveData)bf.Deserialize(file);
+			file.Close();
+
+			PersistantData = data;
+		}
+	}
+}
+
+[System.Serializable]
+public struct SaveData
+{
+	public int HighestLevelComplete;
+	public int AutoCorrectsOwned;
+	public int EmojiCrushOwned;
+	public int Coins;
+	public List<int> HighScores;
+}
---
+++ Match 3 Prototype/Assets/Scripts/Shop.cs
@@ -0,0 +1,99 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Shop : MonoBehaviour {
+
+	public Button AutoCorrect;
+	public Image[] AutoCorectImages;
+	public TextMeshProUGUI[] AutocorectTexts;
+	public Button EmojiCrush;
+	public Image[] EmojiCrushImages;
+	public TextMeshProUGUI[] EmojiCrushTexts;
+
+	public TextMeshProUGUI coinsDisplay;
+
+	private float defaultColor = 1;
+	private float disabledColor = 0.3f;
+
+	private void Awake()
+	{
+		SavingManager.PersistantData.Coins = 100;
+		checkPrices();
+	}
+
+	public void buyAutoCorrect()
+	{
+		SavingManager.PersistantData.Coins -= 100;
+		SavingManager.PersistantData.AutoCorrectsOwned += 1;
+		checkPrices();
+	}
+
+	public void buyEmojiCrush()
+	{
+		SavingManager.PersistantData.Coins -= 10;
+		SavingManager.PersistantData.EmojiCrushOwned += 1;
+		checkPrices();
+	}
+
+	private void checkPrices()
+	{
+		coinsDisplay.text = "Gold: " + SavingManager.PersistantData.Coins;
+		AutoCorrect.interactable = false;
+		EmojiCrush.interactable = false;
+
+		if(SavingManager.PersistantData.Coins >= 100)
+		{
+			AutoCorrect.interactable = true;
+			EmojiCrush.interactable = true;		
+		}
+		else if(SavingManager.PersistantData.Coins >= 10)
+		{
+			AutoCorrect.interactable = false;
+			EmojiCrush.interactable = true;
+		}
+		else
+		{
+			AutoCorrect.interactable = false;
+			EmojiCrush.interactable = false;
+		}
+
+		AutoCorectImages = ChangeImage(AutoCorrect.interactable, AutoCorectImages);
+		AutocorectTexts = ChangeText(AutoCorrect.interactable, AutocorectTexts);
+		EmojiCrushImages = ChangeImage(EmojiCrush.interactable, EmojiCrushImages);
+		EmojiCrushTexts = ChangeText(EmojiCrush.interactable, EmojiCrushTexts);
+	}
+
+	public TextMeshProUGUI[] ChangeText(bool disable, TextMeshProUGUI[] texts)
+	{
+		for(int i = 0; i < texts.Length; i++)
+		{
+			texts[i].color = assignColors(disable, texts[i].color);
+		}
+		return texts;
+	}
+
+	public Image[] ChangeImage(bool Enabled, Image[] images)
+	{
+		for(int i = 0; i < images.Length; i++)
+		{
+			images[i].color = assignColors(Enabled, images[i].color);
+		}
+		return images;
+	}
+
+
+	public Color assignColors(bool Enabled, Color c)
+	{
+		if(!Enabled)
+		{
+			c.a = disabledColor;
+		}
+		else{
+			c.a = defaultColor;
+		}
+		return c;
+	}
+}
