90b48e5cb48d5e52f7b3baa3c256289ab3406330
supaziom6 <adrian.michalowski4@gmail.com>
2019-04-08 12:39:04 +0100

    Created night mode

---
+++ Match 3 Prototype/Assets/nightMode.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class nightMode : MonoBehaviour {
+	public GameObject[] emojiGlows;
+	public GameObject darkener;
+
+	private bool NightModeOn = true;
+	
+	// Update is called once per frame
+	public void toggleNightMode()
+	{
+		NightModeOn = !NightModeOn;
+		foreach(GameObject g in emojiGlows)
+		{
+			g.SetActive(NightModeOn);
+		}
+		darkener.SetActive(NightModeOn);
+
+	}
+}
